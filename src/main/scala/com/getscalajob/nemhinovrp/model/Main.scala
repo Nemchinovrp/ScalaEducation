204377ce627f383fa9b8649fa4afa0f9792cc9ca
nemchinovrp <nemchinovrp@yandex.ru>
2018-06-07 09:00:24 +0300

    init commit

---
+++ src/main/scala/com/getscalajob/nemhinovrp/model/Main.scala
@@ -0,0 +1,7 @@
+package com.getscalajob.nemhinovrp.model
+
+object Main {
+  def main(args: Array[String]): Unit = {
+    println("Hello world");
+  }
+}
---
+++ src/main/scala/com/getscalajob/nemhinovrp/study/Primes.scala
@@ -0,0 +1,6 @@
+package com.getscalajob.nemhinovrp.study
+
+object Primes extends App{
+  def isPrime(n: Int) = (2 until n) forall (n % _ != 0)
+  for (i <- 1 to 100 if isPrime(i)) println(i)
+}
