d89cd937bb765a9e786d2b2b23705340144c99c2
nemchinovrp <nemchinovrp@yandex.ru>
2018-06-07 15:16:33 +0300

    calculate factorial

---
+++ src/main/scala/com/getscalajob/nemhinovrp/study/Factorial.scala
@@ -0,0 +1,15 @@
+package com.getscalajob.nemhinovrp.study
+import java.math.BigInteger
+
+
+object Factorial {
+  def main(args: Array[String]): Unit = {
+    val b = new BigInteger("5")
+    println(factorial(b))
+  }
+  def factorial(x: BigInteger): BigInteger =
+    if (x == BigInteger.ZERO)
+      BigInteger.ONE
+    else
+      x.multiply(factorial(x.subtract(BigInteger.ONE)))
+}
